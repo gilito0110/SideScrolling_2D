286752c3010acc2ebc475a3a59f9ce5307a8b444
gilito0110 <128947701+gilito0110@users.noreply.github.com>
2023-06-04 17:29:28 +0800

    0604

--- Assets/scripts/MenuManager.cs
+++ Assets/scripts/MenuManager.cs
@@ -6,25 +6,30 @@ using System;
 public class MenuManager : MonoBehaviour
 {
     public Animator ani;
-    public AsyncOperation async; 
+    public AsyncOperation async;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     public void StartGame()
-    { 
-        StartCoroutine(Chang());
+    {
+        StartCoroutine(Change());
     }
     public void EndGame()
     {
         Application.Quit();
     }
 
-    IEnumerator Chang()
+
+    IEnumerator Change()
     {
         ani.SetBool("in", true);
         ani.SetBool("out", false);
         yield return new WaitForSeconds(1);
         async=SceneManager.LoadSceneAsync("ingame");
         async.completed += Loaded;
-
     }
 
     private void Loaded(AsyncOperation obj)
