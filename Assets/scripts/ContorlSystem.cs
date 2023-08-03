a29aad8148f595504a529776c625c980af267ea0
gilito0110 <128947701+gilito0110@users.noreply.github.com>
2023-08-03 22:47:08 +0800

    0803 mapOK

--- Assets/scripts/ContorlSystem.cs
+++ Assets/scripts/ContorlSystem.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
 using System;
 
 public class ContorlSystem : MonoBehaviour
@@ -14,13 +12,19 @@ public class ContorlSystem : MonoBehaviour
     [Header("2D 剛體")]
     public Rigidbody2D rig;
 
-    [Header("跳躍次數")]
-    public int maxJumps = 2;
+    [Header("跳躍力量"), Range(0.5f, 50)]
+    public float jumpForce = 10f;// 跳躍力量
 
-    [Header("跳躍力量"), Range(0.5f, 10)]
-    public float jumpForce = 1f;// 跳躍力量
-     [Header("跳躍高度"), Range(0.75f, 3)]
-    public float jumpHeight = 0.8f;             // 跳躍高度
+    [Header("跳躍高度"), Range(0.75f, 10)]
+    public float jumpHeight = 5f;// 跳躍高度
+
+    [Header("動畫控制器")]
+    public Animator ani;
+
+    [Header("跑步參數")]
+    public string PerRun = "IF_Run";
+
+
 
     private bool isJumping = false;
 
@@ -42,6 +46,18 @@ public class ContorlSystem : MonoBehaviour
         // 左右移動
         float moveInput = Input.GetAxis("Horizontal");
         rig.velocity = new Vector2(moveInput * moveSpeed, rig.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.D)|| Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        ani.SetBool("IF_Run", moveInput != 0);
+
     }
 
     private void FixedUpdate()
